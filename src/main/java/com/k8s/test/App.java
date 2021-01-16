f07e9c6c7deb9923ba1049abc1aab27177d956fd
khergner <korhanherguner@gmail.com>
2021-01-16 18:24:08 +0300

    java and maven sample projects

---
+++ src/main/java/com/k8s/test/App.java
@@ -0,0 +1,16 @@
+package com.k8s.test;
+
+/**
+ * @author korhan herguner
+ * DevOps Engineer
+ * https://www.linkedin.com/in/korhan-herguner/
+ * https://medium.com/@korhanherguner
+ * https://github.com/khergner
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Kubernetes!" );
+    }
+}
